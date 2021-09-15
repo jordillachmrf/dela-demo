70338f28fd4468db4a615b435d7cdd7c4b8169d5
jordillachmrf <jordi.llach@marfeel.com>
2021-09-15 23:34:28 +0200

    Initial commit

---
+++ src/main/java/org/acme/getting/started/CallbackResource.java
@@ -0,0 +1,70 @@
+package org.acme.getting.started;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Path("/callback")
+public class CallbackResource {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CallbackResource.class);
+
+    private final Map<String, Map<String, Object>> dirtyCache = new ConcurrentHashMap<>();
+
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void consumeCallBack(Map<String, Object> inputData,
+                                @HeaderParam("log-user-id") String logUserId,
+                                @HeaderParam("log-process-context") String logContextId) {
+        LOGGER.info("Callback received with payload {} with userId {} and processId {}",
+                inputData, logUserId, logContextId);
+        storeInCache(logContextId, inputData);
+    }
+
+    @GET
+    @Path("/heartbeat")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void consumeHeartBeat(@HeaderParam("log-user-id") String logUserId,
+                                 @HeaderParam("log-process-context") String logContextId) {
+        LOGGER.info("HeartBeat received with with userId {} and processId {}", logUserId, logContextId);
+    }
+
+    @GET
+    @Path("/logs")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response exposeLogs() {
+        return Response.ok(dirtyCache).build();
+    }
+
+    @DELETE
+    @Path("/logs")
+    public void deleteLogs() {
+        LOGGER.info("Clearing cache");
+        dirtyCache.clear();
+    }
+
+    private void storeInCache(String logContextId, Map<String, Object> payload) {
+        if (logContextId != null && payload != null) {
+            dirtyCache.put(logContextId, payload);
+        }
+    }
+
+}
