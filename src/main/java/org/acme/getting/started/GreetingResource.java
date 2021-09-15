c8061f91766cbde9c253e829c4338e62d2a00ee8
jordillachmrf <jordi.llach@marfeel.com>
2021-09-15 23:50:31 +0200

    a

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -5,11 +5,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("hello")
 public class GreetingResource {
 
     @GET
-    @Path("/2")
+    @Path("2")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
